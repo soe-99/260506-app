bdc4229efe807cbf023da38548c90f857e70940c
soe-99 <dkfjsleifjl@naver.com>
2026-05-06 08:12:34 +0000

    페이지 만들기

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈페이지 만들기")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
